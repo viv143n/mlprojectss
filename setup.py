85f5b16358524321e885a369655430a38e56b387
viv143n <viv143n@gmail.com>
2023-03-08 16:06:31 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This funciton will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readline()
+        requirements = [req.replace("\n",'') for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    return requirements
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='vivek',
+author_email='viv143n@gmal.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'))
